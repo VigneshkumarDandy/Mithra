a7ed534be43f4d20f99d46b024e83eaba770202e
VigneshkumarDandy <evervignesh@gmail.com>
2017-07-19 12:11:59 +0530

    oddint

---
+++ oddint.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number,a,b;
+scanf("%d%d",&a,&b);
+printf("Enter the Upper and Lower limits:");
+for(number=a;number<=n;number++)
+{
+if(number%2==1)
+printf("%d",number);
+}
+getch();
+}
